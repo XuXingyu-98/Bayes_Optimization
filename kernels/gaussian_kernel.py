76712b4bc2a607e46aab23b2085ab695d13269fe
Luca Grillotti <luca.grillotti16@imperial.ac.uk>
2020-02-11 13:18:36 +0000

    Fixing typo in Kernel Comments

--- kernels/gaussian_kernel.py
+++ kernels/gaussian_kernel.py
@@ -19,7 +19,7 @@ class GaussianKernel(Kernel):
                               Y: np.ndarray,
                               ) -> np.ndarray:
         """
-        :param X: numpy array of size n_1 x l for which each row (x_i) is a data point at which the objective function can be evaluated
+        :param X: numpy array of size n_1 x m for which each row (x_i) is a data point at which the objective function can be evaluated
         :param Y: numpy array of size n_2 x m for which each row (y_j) is a data point at which the objective function can be evaluated
         :return: numpy array of size n_1 x n_2 for which the value at position (i, j) corresponds to the value of
         k(x_i, y_j), where k represents the kernel used.
